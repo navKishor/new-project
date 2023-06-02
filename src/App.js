03fab0ec74b223fa9ef372dfa9e5fbeeb764922e
Novel <vishal.jain@novelveritas.com>
2023-06-02 13:06:09 +0530

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import Form from './Components/from';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App
